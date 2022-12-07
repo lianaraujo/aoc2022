2b99ccc644c42b92df30fb7cf8ed9c56420ad418
Lian <liansutil@gmail.com>
2022-12-07 00:05:05 -0300

    day6 revision

--- src/aoc/day6.clj
+++ src/aoc/day6.clj
@@ -12,3 +12,22 @@
                (conj (apply vector(drop 1 packet)) (first signal))
                (conj packet (first signal)))
              (inc counter)))))
+
+;; fred overflow
+
+(defn part1 
+  [input]
+  (loop [j 4]
+    (let [window (. input substring (- j 4) j)]
+      (if (apply distinct? window)
+        j
+        (recur (inc j))))))
+
+;; another approach
+(defn part2
+  [input]
+  (first 
+   (for [j (range 14 (count input))
+              :let [window (. input substring (- j 14) j)]
+              :when (apply distinct? window)]
+          j)))
